f471030ac05c8fd8102ff4db3b9df870ad1a00e1
Karson Lawson <69886799+Dumbtext@users.noreply.github.com>
2022-11-19 23:09:26 -0600

    Update views.py

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_protect
+
 from .models import points
 import json
 
@@ -11,14 +11,14 @@ def home(request):
     #return HttpResponse('poggers')
 
 
-@csrf_protect
 def newCoords(request):
     if request.method =='POST':
         pointName = request.POST['pointName']
         fieldPoints= request.POST['fieldPoints']
         point = points(pointName= pointName,fieldPoints=json.dump(fieldPoints))
         point.save
-
+    else:
+        print('well fuck')
 
 def getCoords(request):
     
