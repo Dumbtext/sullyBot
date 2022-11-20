ba4411119b754ebba511dae224ae16e50db8a52e
Karson Lawson <69886799+Dumbtext@users.noreply.github.com>
2022-11-19 23:31:34 -0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,13 @@
+
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('newCoords/', views.newCoords, ),
+    
+    path('admin/', admin.site.urls),
+    path('home/', include('Main.urls')),
+    path('newCoords/', include('Main.urls')),
+    path('', RedirectView.as_view(url='/main/'))
 ]
