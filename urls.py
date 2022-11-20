1884317573bf8840a9b1938e96f36dd27bc4e88e
Karson Lawson <69886799+Dumbtext@users.noreply.github.com>
2022-11-19 23:30:55 -0600

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('newCoords/', views.newCoords, name='newCoords'),
+    path('newCoords/', views.newCoords, ),
 ]
